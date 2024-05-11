f5714bfd7418b02f0502649f67ad4fc72a30c8bb
GargulecMizerion <63154385+GargulecMizerion@users.noreply.github.com>
2024-05-12 01:07:30 +0200

    Dodanie "Sprintu"

--- EventHandler.cpp
+++ EventHandler.cpp
@@ -1,20 +1,35 @@
 #include "EventHandler.h"
 
 void EventHandler::keyHandler(GLFWwindow *window, bool keys[1024], float cameraSpeed, glm::vec3 *cameraPos, glm::vec3 cameraFront, glm::vec3 cameraUp, float deltaTime) {
+    float speed;
+
+    // SPRINT
+    if (keys[GLFW_KEY_LEFT_SHIFT]) {
+        speed = cameraSpeed * 3;
+    }
+    else
+    {
+        speed = cameraSpeed;
+    }
+    
     if (keys[GLFW_KEY_W]) {
-        *cameraPos += cameraSpeed * deltaTime * cameraFront;
+        *cameraPos += speed * deltaTime * cameraFront;
     }
     if (keys[GLFW_KEY_S]) {
-        *cameraPos -= cameraSpeed * deltaTime * cameraFront;
+        *cameraPos -= speed * deltaTime * cameraFront;
     }
     if (keys[GLFW_KEY_A]) {
-        *cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed * deltaTime;
+        *cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * speed * deltaTime;
     }
     if (keys[GLFW_KEY_D]) {
-        *cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed * deltaTime;
+        *cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * speed * deltaTime;
     }
+    
+    
 
     if (keys[GLFW_KEY_ESCAPE]) {
         glfwSetWindowShouldClose(window, true);
     }
 }
+
+
